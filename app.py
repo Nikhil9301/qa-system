ed254f03a02c6913eaa8db98c672b6c1ca139ad9
Nikhil9301 <nikhilmaxsingh@gmail.com>
2026-07-12 11:35:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains import RetrievalQA
+from langchain_classic.chains import RetrievalQA
 
 st.set_page_config(page_title="Document QA", page_icon="📄", layout="centered")
 
